e64fc7bc84277d1026cb0271da4fad43fdf2be6a
jaemin.han <jaemin.han@nyu.edu>
2018-04-06 14:41:52 -0400

    functionally working but cannot find onclick error - in progress

--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -5,9 +5,9 @@ import Pet from './Pet';
 class PetBrowser extends React.Component {
   render() {
 
-    const pets = this.props.pets.map((pet, index) => {
+    const pets = this.props.pets.map((pet, index) => (
       <Pet pet={pet} key={index} onAdoptPet={this.props.onAdoptPet} isAdpoted={this.props.adoptedPets.includes(pet.id)} />
-    })
+    ));
 
     return (
       <div className="ui cards">
